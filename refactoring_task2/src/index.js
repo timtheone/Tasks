362e1177dd9b145e21c279da338b516141c0f8b6
Tim Almamedov <tim.almamedov@gmail.com>
2018-09-08 14:47:04 +0200

    delete folder refactor task 2

--- refactoring_task2/src/index.js
+++
@@ -1,6 +0,0 @@
-require('./sass/main.scss');
-import showTabs from './js/main.js';
-
-const listParent = document.querySelector('.tab-navs__list');
-
-listParent.addEventListener("click", showTabs, false);
